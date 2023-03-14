7def56cbc0a93f4d5bd7d33e82b967fb8b9ac209
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-14 19:17:09 +0100

    Update task1.py

--- Exercises/Conditional Statements/task1.py
+++ Exercises/Conditional Statements/task1.py
@@ -1,2 +1,18 @@
+#!/usr/bin/python3
 # Write a program that takes an integer as input and prints whether it is positive, negative, or zero.
 # Write your program below
+
+
+number = 0
+while True:
+    try:
+        number = int(input("Enter Number: "))
+        break
+    except:
+        print("Numeric value required.")
+if number == 0:
+    print(f"{number:d} is zero")
+elif number < 0:
+    print(f"{number:d} is Negative.")
+else:
+    print(f"{number:d} is Positive.")
